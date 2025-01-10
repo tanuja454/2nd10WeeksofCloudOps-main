a2020a6fb837ab82ee379dcb588cb84ac00feffc
tanuja454 <chennapragadalaxminagatanuja@gmail.com>
2025-01-10 16:11:03 +0530

    Update config.js

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -1,3 +1,3 @@
 // const API_BASE_URL = "http://localhost:8800";
-const API_BASE_URL = "http://api.nikkionline.shop";
+const API_BASE_URL = "http://a185559b97fe24aa9911109f786755f7-137237656.us-west-1.elb.amazonaws.com";
 export default API_BASE_URL;
