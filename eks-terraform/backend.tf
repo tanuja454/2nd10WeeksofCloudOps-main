4f549d06a1b23a240c906145e04bc1e3e551ceec
tanuja454 <chennapragadalaxminagatanuja@gmail.com>
2025-06-08 19:25:42 +0530

    Update backend.tf

--- eks-terraform/backend.tf
+++ eks-terraform/backend.tf
@@ -9,7 +9,7 @@ terraform {
   backend "s3" {
     bucket = "kotakkkkk"
     key    = "ammu/terraform.tfstate"
-    region = "us-west-1"
+    region = "us-east-1"
   }
 
   required_version = ">= 1.6.3"
